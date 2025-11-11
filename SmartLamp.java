40b04d15feb907ca0b287d08edf505a1bb6cc613
Maulanahb <maulanahmad2287@gmail.com>
2025-11-11 22:38:37 +0700

    Implement SmartLamp class with WiFi connectivity and main functionality

--- SmartLamp.java
+++ SmartLamp.java
@@ -1,3 +1,16 @@
-public class SmartLamp {
-    
+public class SmartLamp extends PerangkatRumah implements IConnectable {
+
+    public SmartLamp(String nama, int daya) {
+        super(nama, daya);
+    }
+
+    @Override
+    public void fungsiUtama() {
+        System.out.println(nama + " menyala dengan terang hangat.");
+    }
+
+    @Override
+    public void connectToWiFi() {
+        System.out.println(nama + " terhubung ke WiFi untuk kontrol jarak jauh.");
+    }
 }
