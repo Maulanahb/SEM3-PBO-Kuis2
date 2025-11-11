bdbfea9301c94e0a94cba434fd4d7abec0b9b08c
Maulanahb <maulanahmad2287@gmail.com>
2025-11-11 22:39:15 +0700

    Refactor MainSmartHome and SmartOven classes to implement full functionality for smart home devices

--- MainSmartHome.java
+++ MainSmartHome.java
@@ -1,3 +1,37 @@
 public class MainSmartHome {
-    
+    public static void main(String[] args) {
+        SmartSpeaker speaker = new SmartSpeaker("Google Home", 20);
+        SmartLamp lamp = new SmartLamp("Philips Hue", 10);
+        SmartOven oven = new SmartOven("Smart Oven LG", 800);
+
+        System.out.println("=== Sistem Rumah Pintar Dihidupkan ===\n");
+
+        speaker.hidupkan();
+        lamp.hidupkan();
+        oven.hidupkan();
+
+        System.out.println();
+
+        speaker.connectToWiFi();
+        lamp.connectToWiFi();
+
+        System.out.println();
+
+        speaker.fungsiUtama();
+        lamp.fungsiUtama();
+        oven.fungsiUtama();
+
+        System.out.println();
+
+        speaker.naikkanVolume();
+        speaker.turunkanVolume();
+
+        System.out.println();
+
+        speaker.matikan();
+        lamp.matikan();
+        oven.matikan();
+
+        System.out.println("\n=== Semua perangkat dimatikan ===");
+    }
 }
--- SmartOven.java
+++ SmartOven.java
@@ -1,3 +1,11 @@
-public class SmartOven {
-    
+public class SmartOven extends PerangkatRumah {
+
+    public SmartOven(String nama, int daya) {
+        super(nama, daya);
+    }
+
+    @Override
+    public void fungsiUtama() {
+        System.out.println(nama + " sedang memanggang makanan dengan suhu optimal.");
+    }
 }
