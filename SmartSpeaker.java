39d58f2b392ab952238e95c3065e052843a99e0c
Maulanahb <maulanahmad2287@gmail.com>
2025-11-11 22:38:18 +0700

    Implement SmartSpeaker class with volume control and WiFi connectivity

--- SmartSpeaker.java
+++ SmartSpeaker.java
@@ -1,3 +1,30 @@
-public class SmartSpeaker {
-    
+public class SmartSpeaker extends PerangkatRumah implements IConnectable, IAudible {
+    private int volume;
+
+    public SmartSpeaker(String nama, int daya) {
+        super(nama, daya);
+        this.volume = 5;
+    }
+
+    @Override
+    public void fungsiUtama() {
+        System.out.println(nama + " memainkan musik favorit Anda.");
+    }
+
+    @Override
+    public void connectToWiFi() {
+        System.out.println(nama + " terhubung ke WiFi.");
+    }
+
+    @Override
+    public void naikkanVolume() {
+        if (volume < 10) volume++;
+        System.out.println(nama + " volume naik ke " + volume);
+    }
+
+    @Override
+    public void turunkanVolume() {
+        if (volume > 0) volume--;
+        System.out.println(nama + " volume turun ke " + volume);
+    }
 }
